1a99f8b1884c7db6c5b6c54104e14d2dfd26cbb1
airtoxin <airtoxin@icloud.com>
2017-03-21 08:00:52 +0900

    Refine

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -3,21 +3,16 @@ import webpack from 'webpack';
 import Copy from 'copy-webpack-plugin';
 
 export default {
-  entry: './src/index.jsx',
+  entry: './src/index.js',
   output: {
     publicPath: '/',
-    sourcePath: ' ',
-    path: path.join(__dirname, 'public'),
+    path: path.join(__dirname, 'dist'),
     filename: 'app.js',
   },
   target: 'web',
-  resolve: {
-    extensions: ['', '.js', '.jsx'],
-  },
   module: {
-    loaders: [
-      { test: /\.jsx?$/, include: [path.resolve(__dirname, 'src')], loader: 'babel-loader' },
-      { test: /\.css$/, include: [path.resolve(__dirname, 'src')], loaders: ['style', 'css?modules'] },
+    rules: [
+      { test: /\.js$/, include: [path.resolve(__dirname, 'src')], loader: 'babel-loader' },
     ],
   },
   plugins: [
@@ -26,12 +21,8 @@ export default {
     ]),
     new webpack.HotModuleReplacementPlugin(),
   ],
-  devTool: 'inline-source-map',
+  devtool: 'cheap-module-source-map',
   devServer: {
-    contentBase: 'public',
     port: 9000,
-    inline: true,
-    hot: true,
-    colors: true,
   },
 };
