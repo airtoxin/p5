e7fe433ca73b3dfe140e338f4e8b013821987c2c
airtoxin <airtoxin@icloud.com>
2017-03-21 19:41:58 +0900

    Integrate with p5.js

--- src/pages/Hoge.js
+++ src/pages/Hoge.js
@@ -1,6 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
+import P5 from 'p5';
 
-export default (props) => {
-  console.log("@props", props);
-  return (<h1>hoge</h1>);
-};
+export default class Hoge extends Component {
+  constructor() {
+    super();
+
+    this.id = '' + Math.random();
+  }
+
+  componentDidMount() {
+    new P5((p) => {
+      p.setup = () => this.p5setup(p);
+      p.draw = () => this.p5draw(p);
+    }, this.id);
+  }
+
+  render() {
+    return (<div id={this.id}/>);
+  }
+
+  p5setup(p) {
+    p.createCanvas(600, 400);
+  }
+
+  p5draw(p) {
+    p.background(0);
+    p.rect(10, 10, 100, 100);
+  }
+}
