36ad0b7ad1247d3b79a3a7f5f23bffa2d19e9610
airtoxin <airtoxin@icloud.com>
2017-03-21 16:48:31 +0900

    Normalize

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -18,6 +18,7 @@ export default {
   plugins: [
     new Copy([
       { from: 'src/index.html' },
+      { from: 'src/normalize.css' },
     ]),
     new webpack.HotModuleReplacementPlugin(),
   ],
