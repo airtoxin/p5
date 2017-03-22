92783d85f06b39282f78f49184d6d2e070c85edf
airtoxin <airtoxin@icloud.com>
2017-03-22 18:04:52 +0900

    Add slider

--- src/components/pages/Branches/index.js
+++ src/components/pages/Branches/index.js
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
+import Slider from 'material-ui/Slider';
 import P5Canvas from '@/P5Canvas';
 import Branch from './Branch';
 import * as strategies from './strategies';
@@ -20,26 +21,47 @@ const draw = (n, branchingStrategy) => p => {
 export default class Branches extends Component {
   constructor() {
     super();
-    this.state = { name: 'spidersWebDrips' };
+    this.state = {
+      n: 6,
+      name: 'spidersWebDrips'
+    };
 
-    this.handleChange = this.handleChange.bind(this);
+    this.handleChangeName = this.handleChangeName.bind(this);
+    this.handleChangeN = this.handleChangeN.bind(this);
   }
 
-  handleChange(event, index, value) {
-    this.setState({ name: value });
+  handleChangeName(event, index, name) {
+    this.setState({
+      ...this.state,
+      name,
+    });
+  }
+
+  handleChangeN(event, n) {
+    this.setState({
+      ...this.state,
+      n
+    });
   }
 
   render() {
     return (
       <div>
-        <SelectField value={this.state.name} onChange={this.handleChange}>
+        <SelectField value={this.state.name} onChange={this.handleChangeName}>
           <MenuItem value="spidersWebDrips" primaryText="Spider's web drips" />
           <MenuItem value="stationMap" primaryText="Station map" />
         </SelectField>
         <P5Canvas
           name={this.state.name}
           setup={setup}
-          draw={draw(6, strategies[`${this.state.name}Strategy`])}
+          draw={draw(this.state.n, strategies[`${this.state.name}Strategy`])}
+        />
+        <Slider
+          min={0}
+          max={10}
+          step={1}
+          value={this.state.n}
+          onChange={this.handleChangeN}
         />
       </div>
     );
